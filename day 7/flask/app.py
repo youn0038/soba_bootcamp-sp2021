19c1d74218905262ffa7c46704522d5320f7121d
Anna Young <annayoung@Annas-MBP.attlocal.net>
2021-03-04 08:38:49 -0600

    update day7/8 materials

--- day 7/flask/app.py
+++ day 7/flask/app.py
@@ -10,7 +10,34 @@ def home():
 
 @app.route('/add', methods=["GET", "POST"])
 def todo_add():
+    if request.method == 'POST':
+        new_todo = {
+            'id': todo_items[0],
+            'title': request.form['title'],
+            'date': request.form['date'],
+            'description': request.form['description']
+        }
+        #add to list / save in database
+        todo_items.append(new_todo)
+        #update the identifier
+        todo_items[0] = todo_items[0] + 1
     return render_template('todo_add.html')
-    # if request.method == 'POST':
 
-@app.route('/todo-list')
+@app.route('/todo-list', methods=["GET"])
+def todo_list():
+    return render_template('todo_list.html', todo_items=todo_items)
+
+# Delete
+@app.route('/todo-list/delete', methods=["DELETE"])
+def todo_item():
+    req_response = request.get_json()
+    req_id = int(req_response['id'])
+    for item in todo_items[1:]:
+        if item['id'] == req_id:
+            todo_items.remove(item)
+    print(todo_items)
+    return str(req_id)
+
+@app.route('/example')
+def example_1():
+    return render_template('get.html')
